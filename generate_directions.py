5154c954dc8c8c07c9f478673c949607a055ac0f
Lindsay Marean <lmarean@bensay.org>
2020-10-12 20:48:46 -0700

    figures cumulative distance and adds to dataframe

--- generate_directions.py
+++ generate_directions.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python3
 
-# import pandas
+import pandas
 from geopy import distance
+from GpxParser import GpxParse
 from FakeDataFrames import GenDataFrames
+import datetime
+import time
+import gpxpy
+import requests
 
 
-def generate_directions(frame: list) -> str:
+def generate_directions(dataframe):
+    print(dataframe["Longitude"][3], dataframe["Latitude"][3])
 
-    # test ability to read from DataFrame object
+    # dataframe.apply(lambda r: distance.distance(
+    #     ["Latitude", "Longitude"]), axis=1)
+    # print(dataframe)
 
-    # make an array of turn dictionaries
-    # keep original DataFrame and modify as needed
-        # dictionary fields:
-        # turn_lat (latitude of mid-point of turn)
-        # turn_long
-        # street
-        # type (left, right, straight)
-        # notes ("Turn right onto Franklin")
-        # cum_dist (cumulative distance since start)
-    turn = {"turn_lat": frame[0]["lat"], "turn_long": frame[0]["long"],
-            "type": "Start", "notes": "Start of route", "cum_dist": 0.0}
-    instructions = []
-    instructions.append(turn)
+    # cumulative distance at start is 0
+    # kludge to make a Distance instance of 0
+    cumul_distance = [distance.distance((44, -123), (44, -123))]
+    for datapoint in range(1, len(dataframe["Longitude"])):
+        cumul_distance.append(distance.distance((dataframe["Latitude"][datapoint], dataframe["Longitude"][datapoint]), (
+            dataframe["Latitude"][datapoint-1], dataframe["Longitude"][datapoint-1])))
+        cumul_distance[datapoint] += cumul_distance[datapoint-1]
 
-    # convert lat and long into UTM coordinates
-    # may not be necessary with geopy
+    dataframe["Distance"] = cumul_distance
 
-    # calculate distance between turns
-
-    # rough calculate experiment: from post-turn to pre-turn
-    point1 = (frame[0]["lat"], frame[1]["long"])
-    point2 = (frame[2]["lat"], frame[3]["long"])
-    point3 = (frame[4]["lat"], frame[5]["long"])
-    print("Hayes: ", point1, " 18th: ", point2, " Chambers: ", point3)
-    distance1 = distance.distance(point1, point2)
-    distance2 = distance.distance(point2, point3)
-    print("distance1: ", distance1, "distance2: ", distance2)
-
-    # figure more precise distance in km from one turn coordinate to the next
-    # write to dictionary
+    hardcode = ["Hayes", "", "", "", "", "", "", "", "", "", "", "Hayes", "18th", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "18th", "Chambers", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Chambers", "14th", "", "", "", "", "", "", "", "", "", "", "",
+                "", "", "14th", "Tyler", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Tyler", "5th", "", "", "", "", "", "", "", "", "", "5th", "Blair", "", "", "Blair"]
+    dataframe["Street"] = hardcode
+    print(dataframe)
 
     # calculate direction of turn
 
@@ -47,34 +39,10 @@ def generate_directions(frame: list) -> str:
     # write to CSV file from dictionary
 
 
-def make_data():
-
-    mockDict1 = {"index": 1, "lat": 43.0891310, "long": -
-                 122.1118960, "time": "17:15:31", "street": "Hayes"}
-    mockDict2 = {"index": 2, "lat": 43.0889390, "long": -
-                 122.1162580, "time": "17:16:29", "street": "Hayes"}
-    mockDict3 = {"index": 3, "lat": 43.0885860, "long": -
-                 122.1165880, "time": "17:16:35", "street": "18th"}
-    mockDict4 = {"index": 4, "lat": 43.0780780, "long": -
-                 122.1179230, "time": "17:19:15", "street": "18th"}
-    mockDict5 = {"index": 5, "lat": 43.0778710, "long": -
-                 122.1179050, "time": "17:19:20", "street": "Chambers"}
-    mockDict6 = {"index": 6, "lat": 43.0747080, "long": -
-                 122.1184860, "time": "17:21:04", "street": "Chambers"}
-    mockArr = [mockDict1, mockDict2, mockDict3,
-               mockDict4, mockDict5, mockDict6]
-
-    print("mockDict4['lat']: ", mockDict4["lat"])
-    print("mockArr[4]['lat']", mockArr[4]["lat"])
-
-    return mockArr
-
-
 def main():
-    dataFrame = GenDataFrames()
+    dataFrame = GpxParse("Morning_Ride.gpx")
     print(dataFrame)
-    dataPoints = make_data()
-    generate_directions(dataPoints)
+    generate_directions(dataFrame)
 
 
 if __name__ == "__main__":
