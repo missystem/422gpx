466314cc522579fa197cc5d2245a4b55f225d306
Xuehai Zhou <xuehaizhou9@gmail.com>
2020-10-23 01:25:56 -0700

    module 2 done

--- googleLookup.py
+++ googleLookup.py
@@ -1,13 +1,48 @@
+import sys
 import requests
 import math
 import json
 
 
-def hasNumbers(inputString):
-    return any(char.isdigit() for char in inputString)
+def hasNumbers(String):
+    return any(char.isdigit() for char in String)
+
+
+def sameStreet(street1, street2):
+    list1 = street1.split('&')
+    list2 = street2.split('&')
+    for item in list1:
+        if item in list2:
+            return True
+
+    set1 = street1.split(' ')
+    set2 = street2.split(' ')
+    remove_list = ["Dr", "Drive", "Ave", "Rd", "Road", "PI", "St", "Cir", "Blvd",
+                   "NW", "SW", "NE", "SE", "S", "N", "E", "W"]
+    for item in remove_list:
+        if item in set1:
+            set1.remove(item)
+        if item in set2:
+            set2.remove(item)
+    string1 = ""
+    string2 = ""
+    for item in set1:
+        string1 += item + " "
+    for item in set2:
+        string2 += item + " "
+    if string1 == string2:
+        return True
+
+    is_same = 0
+    for item in set1:
+        if item in set2:
+            is_same += 1
+    if is_same >= 2:
+        return True
+    return False
+
 
 def get_address(df, dataFrame, api_key, index, global_index):
-    df_len = len(df)
     lon = df.iloc[index][0]
     lat = df.iloc[index][1]
     latlng = str(lat) + "," + str(lon)
@@ -58,7 +93,8 @@ def binarySearch(df, dataFrame, api_key):
     if len(df) <= 2 and start_address != end_address:
         return None
 
-    elif start_address == mid_address and mid_address == end_address:
+    elif (start_address == mid_address and mid_address == end_address) or (
+            sameStreet(start_address, mid_address) and sameStreet(mid_address, end_address)):
 
         if local_end - local_start < 2000:  # safe interval can be vary
             return None
@@ -77,9 +113,21 @@ def df_cleanup(df):
     for i in range(1, df_len):
         if i == df_len - 1:
             break
-        if df.iloc[i]['Street'] == buffer or df.iloc[i]['Street'] == "":
+        if df.iloc[i]['Street'] == buffer or sameStreet(df.iloc[i]['Street'], buffer) or df.iloc[i]['Street'] == "":
             df.at[i, 'Street'] = ""
         else:
             df.at[i-1, 'Street'] = buffer
             buffer = df.iloc[i]['Street']
     return df
+
+
+def main(dataFrame, api_key):
+    dataFrame["Street"] = ""
+    binarySearch(dataFrame, dataFrame, api_key)   # "AIzaSyCkeuvNkYicjuLSRcy_nP5Or0yH5lRulCE"  for testing
+    df_cleanup(dataFrame)
+    return dataFrame
+
+
+if __name__ == '__main__':
+    api_key = sys.argv[1]
+    main(api_key)
