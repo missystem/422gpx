47c8552242f68fc8d9eb016a1c4655c60aebb2bf
Yuhan Shi <yuhans@protonmail.com>
2020-10-23 12:59:00 -0700

    added Google API lookup

--- skeleton.py
+++ skeleton.py
@@ -6,6 +6,7 @@ from geopy import distance  # for distance calculation
 from GpxParser import GpxParse
 from generate_directions import generate_directions
 from addressLookup import *
+from googleLookup import *
 import sys
 import datetime
 import time
@@ -28,9 +29,17 @@ def runner(path, api_key):
     dataFrame = dataFrame.reset_index(drop=True)
     # lookup street name for each data point
     binarySearch(dataFrame, dataFrame, api_key)
+    # df_len = len(dataFrame)
+    # print(df_len)
+    # for i in range(df_len):
+    #     print(dataFrame.iloc[i][2])
     # for a contiguous series of data points with the same street, eliminate all but the first and last
     # return the revised pandas DataFrame
     filtered = df_cleanup(dataFrame)
+    df_len = len(filtered)
+    print(df_len)
+    for i in range(df_len):
+        print(filtered.iloc[i][2])
     # figure cumulative distance from turn to turn
     # determine direction of turn (left or right)
     # compose string like "make a right turn onto Franklin Blvd."
@@ -40,6 +49,42 @@ def runner(path, api_key):
     # cue_sheet.to_html(buf="result.html")
 
     return cue_sheet    # dataframe
+
+
+def runner2(path, api_key):
+    # Same thing as runner, but uses GoogleAPI for the second module.
+    dataFrame = GpxParse(path)  # some other files available
+    # add an empty street name column
+    dataFrame["Street"] = ""
+    # scope with 300 data points
+    dataFrame = dataFrame.iloc[0:300]
+    # re-set the dataFrame's index
+    # binary search highly rely on correct index
+    dataFrame = dataFrame.reset_index(drop=True)
+    # lookup street name for each data point
+    binarySearch2(dataFrame, dataFrame, api_key)
+    # df_len = len(dataFrame)
+    # print(df_len)
+    # for i in range(df_len):
+    #     print(dataFrame.iloc[i][2])
+    # for a contiguous series of data points with the same street, eliminate all but the first and last
+    # return the revised pandas DataFrame
+    filtered = df_cleanup2(dataFrame)
+    df_len = len(filtered)
+    print(df_len)
+    for i in range(df_len):
+        print(filtered.iloc[i][2])
+    # figure cumulative distance from turn to turn
+    # determine direction of turn (left or right)
+    # compose string like "make a right turn onto Franklin Blvd."
+    # write instructions to CSV
+    cue_sheet = generate_directions(filtered)
+    
+    # cue_sheet.to_html(buf="result.html")
+
+    return cue_sheet    # dataframe
+
+
 
 # def main(path, api_key):
 #     runner(path, api_key)
